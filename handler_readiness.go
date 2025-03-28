7a6b864341961873c47d05f8c46025e97c42e58c
CheSergo <serg.chernov.job@gmail.com>
2025-03-28 23:17:11 +0300

    add user model as test

--- handler_readiness.go
+++ handler_readiness.go
@@ -1,7 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	// fmt.Printf("%+v\n", r)
+	// fmt.Printf("Method: %v\n", r.Method)
+	// fmt.Printf("Url: %v\n", r.URL)
 	respondWithJSON(w, 200, struct{}{})
 }
--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/CheSergo/rssagg/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
@@ -17,6 +25,29 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT os not found in the env file")
+	}
+
+	// Получаем переменные окружения
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dbURL := "postgres://" + dbUser + ":" + dbPassword + "@" + dbHost + ":" + dbPort + "/" + dbName + "?sslmode=disable"
+	fmt.Println(dbURL)
+
+	if len(dbURL) < 16 {
+		log.Fatal("dbURL is not found in .env file")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database")
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -32,6 +63,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -41,7 +73,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
